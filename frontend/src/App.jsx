a783e38d6bb2ea06fc9bfefb021ceb7bb340ff03
xaco04 <xavivico04@gmail.com>
2026-02-03 18:37:18 +0100

    añadido seleccionar projects

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,37 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import ProjectsPage from './pages/ProjectsPage';
+import { useEffect, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectsPage from "./pages/ProjectsPage";
+import { api } from "./api";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    api.get("/projects").then(res => {
+      setProjects(res.data);
+      if (!selected && res.data.length) setSelected(res.data[0]);
+    });
+  }, []);
+
   return (
     <Container fluid>
       <Row>
         <Col md={3} lg={2} className="bg-light vh-100 p-0">
-          {/* Sidebar */}
-          <ProjectsPage sidebarOnly />
+          <ProjectsPage
+            sidebarOnly
+            projects={projects}
+            selected={selected}
+            setSelected={setSelected}
+          />
         </Col>
 
         <Col md={9} lg={10} className="p-3">
-          <ProjectsPage />
+          <ProjectsPage
+            projects={projects}
+            selected={selected}
+            setSelected={setSelected}
+          />
         </Col>
       </Row>
     </Container>
--- frontend/src/pages/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage.jsx
@@ -5,22 +5,12 @@ import Sidebar from '../components/Sidebar';
 import TaskForm from '../components/TaskForm';
 import TaskList from '../components/TaskList';
 
-export default function ProjectsPage({ sidebarOnly }) {
-  const [projects, setProjects] = useState([]);
-  const [selected, setSelected] = useState(null);
+export default function ProjectsPage({ sidebarOnly, projects, selected, setSelected }) {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    api.get('/projects').then(res => {
-      setProjects(res.data);
-      if (!selected) setSelected(res.data[0]);
-    });
-  }, []);
-
-  useEffect(() => {
     if (selected && !sidebarOnly) {
-      api.get(`/tasks?projectId=${selected._id}`)
-        .then(res => setTasks(res.data));
+      api.get(`/tasks?projectId=${selected._id}`).then(res => setTasks(res.data));
     }
   }, [selected]);
 
@@ -43,19 +33,17 @@ export default function ProjectsPage({ sidebarOnly }) {
       <TaskForm
         projectId={selected._id}
         onCreated={() =>
-          api.get(`/tasks?projectId=${selected._id}`)
-            .then(res => setTasks(res.data))
+          api.get(`/tasks?projectId=${selected._id}`).then(res => setTasks(res.data))
         }
       />
 
       <TaskList
         tasks={tasks}
         refresh={() =>
-          api.get(`/tasks?projectId=${selected._id}`)
-            .then(res => setTasks(res.data))
+          api.get(`/tasks?projectId=${selected._id}`).then(res => setTasks(res.data))
         }
       />
-
     </>
   );
 }
+
